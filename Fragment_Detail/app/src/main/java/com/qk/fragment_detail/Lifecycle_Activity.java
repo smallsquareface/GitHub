589220012397397406151870c395acb197be5cce
smallsquareface <Jerry_qinkai@163.com>
2018-09-05 22:15:10 +0800

    Replace with app Fragment with V4 Fragment

--- Fragment_Detail/app/src/main/java/com/qk/fragment_detail/Lifecycle_Activity.java
+++ Fragment_Detail/app/src/main/java/com/qk/fragment_detail/Lifecycle_Activity.java
@@ -1,5 +1,6 @@
 package com.qk.fragment_detail;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
--- Fragment_Detail/app/src/main/java/com/qk/fragment_detail/Static_Method_Activity.java
+++ Fragment_Detail/app/src/main/java/com/qk/fragment_detail/Static_Method_Activity.java
@@ -3,8 +3,9 @@ package com.qk.fragment_detail;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 
-public class Static_Method_Activity extends Activity {
+public class Static_Method_Activity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
